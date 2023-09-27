87c92ea2467b1eda3cd9f85a2f764df5f5156086
Adam Ousmer <aousmer03@gmail.com>
2023-09-27 10:35:46 -0400

    Set up the GUI directory and main files.
    Took 39 seconds

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,6 +1,6 @@
 """
 ******************************************************************
-Copyright Adam Ousmer for Space Concordia: Rocketry Division 2023
+Copyright Adam Ousmer for Space Concordia - Rocketry Division 2023
 All Rights Reserved.
 ******************************************************************
 
@@ -8,4 +8,15 @@ This module contains the code for the GUI and it is use for all user's interacti
 of the dicomdir file when the Scan object is being initialized.
 """
 
+from flask import Flask, render_template
 
+def main():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    app.run(debug=True)
+
+main()
