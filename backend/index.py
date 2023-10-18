f6d9dedc914228c481f5f6101ec4f85381f80230
Adam Ousmer <aousmer03@gmail.com>
2023-10-18 09:57:19 -0400

    Load buttons
    Took 1 hour 45 minutes

--- backend/index.py
+++ backend/index.py
@@ -17,6 +17,16 @@ def load_existing_scans():
     pass
 
 
+def restore_state():
+    # This will be used when we want to open an existing scan already analyzed.
+    pass
+
+
+def restore_scan():
+    # This will be used when we want to open an existing scan already analyzed.
+    pass
+
+
 # TODO add a module for the database
 # TODO add a module for the GUI
 
@@ -34,3 +44,6 @@ for j in tqdm(Scans.scans._head.data[40 if Scans.scans._head.key == "FLASH" else
     ax.axis([0, j.pixel_array_HU.shape[1], j.pixel_array_HU.shape[0], 0])
 
     plt.show()
+
+if __name__ == '__main__':
+    restore_state()
